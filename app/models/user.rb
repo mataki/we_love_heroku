1a77635a107e533fed11c6dc6e271e3b9abd1809
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-09-28 21:24:26 +0900

    空白を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :image, :default_provider_id
-  
+
   has_many :providers_users, :dependent => :destroy
   has_many :providers, :through => :providers_users
   has_many :sites, :dependent => :destroy
-  
+
   def self.find_for_facebook_oauth(auth, current_user = nil)
     providers_user = ProvidersUser.find_by_provider_id_and_user_key Provider.facebook.id, auth['uid'].to_s
     begin
@@ -62,14 +62,14 @@ class User < ActiveRecord::Base
     end
     user
   end
-  
+
   def self.find_for_twitter_oauth(auth, current_user = nil)
     providers_user = ProvidersUser.find_by_provider_id_and_user_key Provider.twitter.id, auth['uid'].to_s
 
     name = auth['info']['nickname']
     image = auth['info']['image']
     email = "#{auth['uid']}@twitter.example.com" # twitter return no email, so set dummy email address because of email wanne be unique.
-    
+
     if providers_user.nil?
       if current_user.nil?
         user = User.create!({
@@ -103,7 +103,7 @@ class User < ActiveRecord::Base
         user.image = image
         user.save!
       end
-      
+
       providers_user.name = name
       providers_user.image = image
       providers_user.email = email
@@ -113,13 +113,13 @@ class User < ActiveRecord::Base
     end
     user
   end
-  
+
   def self.find_for_github_oauth(auth, current_user = nil)
     providers_user = ProvidersUser.find_by_provider_id_and_user_key Provider.github.id, auth['uid'].to_s
     name = auth['info']['nickname']
     image = auth['extra']['raw_info']['avatar_url']
     email = auth['info']['email']||"#{auth['uid']}@github.example.com"
-    
+
     if providers_user.nil?
       if current_user.nil?
         user = User.create!({
@@ -152,7 +152,7 @@ class User < ActiveRecord::Base
         user.image = image
         user.save!
       end
-      
+
       providers_user.name = name
       providers_user.image = image
       providers_user.email = email
@@ -161,24 +161,24 @@ class User < ActiveRecord::Base
     end
     user
   end
-  
+
   def self.find_by_path provider_name, user_key
     providers_user = ProvidersUser.where(:provider_id => Provider.send(provider_name).id, :user_key => user_key).first
     self.includes(:sites).find providers_user.user_id
   end
-  
+
   def user_key
     self.providers_users.where(:provider_id => self.default_provider_id).first.user_key
   end
-  
+
   def default_provider
     Provider.select('id, name').find self.default_provider_id
   end
-  
+
   def has_provider? provider_id
     self.providers_users.select(:provider_id).map{|providers_user|providers_user.provider_id}.include? provider_id
   end
-  
+
   def has_all_provider?
     self.providers_users.length === Provider.all.length
   end
