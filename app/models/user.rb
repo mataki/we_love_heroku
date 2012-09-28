6a2bab35c9e79cbb93285db6acefd1dd1d980243
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-09-28 22:06:20 +0900

    auth の引数をフラットに展開

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,13 @@ class User < ActiveRecord::Base
       image = auth['info']['image'].gsub(/(type=)(.*)/, '\1')
     end
     email = auth['info']['email']
+    uid = auth['uid'].to_s
+    token = auth['credentials']['token']
+    secret = auth['credentials']['secret']
+
     logger.info auth.to_yaml
-    user = get_user_from_hoge(providors_user, current_user, auth, name, image, email, Provider.twitter.id)
+
+    user = get_user_from_hoge(providors_user, current_user, Provider.twitter.id, auth, name, image, email, uid, token, seret)
   end
 
   def self.find_for_twitter_oauth(auth, current_user = nil)
@@ -33,8 +38,11 @@ class User < ActiveRecord::Base
     name = auth['info']['nickname']
     image = auth['info']['image']
     email = "#{auth['uid']}@twitter.example.com" # twitter return no email, so set dummy email address because of email wanne be unique.
+    uid = auth['uid'].to_s
+    token = auth['credentials']['token']
+    secret = auth['credentials']['secret']
 
-    user = get_user_from_hoge(providors_user, current_user, auth, name, image, email, Provider.twitter.id)
+    user = get_user_from_hoge(providors_user, current_user, Provider.twitter.id, auth, name, image, email, uid, token, secret)
   end
 
   def self.find_for_github_oauth(auth, current_user = nil)
@@ -42,11 +50,14 @@ class User < ActiveRecord::Base
     name = auth['info']['nickname']
     image = auth['extra']['raw_info']['avatar_url']
     email = auth['info']['email']||"#{auth['uid']}@github.example.com"
+    uid = auth['uid'].to_s
+    token = auth['credentials']['token']
+    secret = auth['credentials']['secret']
 
-    user = get_user_from_hoge(providors_user, current_user, auth, name, image, email, Provider.github.id)
+    user = get_user_from_hoge(providors_user, current_user, Provider.github.id, uid, name, image, email, uid, token)
   end
 
-  def self.find_or_create_from_auth_infos(providors_user, current_user, auth, name, image, email, provider_id)
+  def self.find_or_create_from_auth_infos(providors_user, current_user, provider_id, name, image, email, uid, token, secret = nil)
     if providers_user.nil?
       user = current_user || User.create!({
         :password => Devise.friendly_token[0,20],
@@ -70,9 +81,9 @@ class User < ActiveRecord::Base
     providors_user ||= ProvidersUser.new
     providors_user.provider_id = provider_id
     providors_user.user_id = user.id
-    providors_user.user_key = auth['uid'].to_s
-    providors_user.access_token = auth['credentials']['token']
-    providors_user.secret = auth['credentials']['secret']
+    providors_user.user_key = uid
+    providors_user.access_token = token
+    providors_user.secret = secret
     providors_user.name = name
     providors_user.email = email
     providors_user.image = image
