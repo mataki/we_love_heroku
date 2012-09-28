f61349a4e7008c80ed8dfad7f6cb9b7a931e3410
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-09-28 12:01:51 +0900

    スペースを統一

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Gallery::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :skip => [:sessions]
   devise_scope :user do
-    match '/users/sign_out(.:format)', {:action=>"destroy", :controller=>"devise/sessions", :via => :delete, :as => :destroy_user_session }
-    match '/users/login', {:action=>"login", :controller=>"users", :via => :get, :as => :new_user_session }
+    match '/users/sign_out(.:format)', {:action => "destroy", :controller => "devise/sessions", :via => :delete, :as => :destroy_user_session }
+    match '/users/login', {:action => "login", :controller=>"users", :via => :get, :as => :new_user_session }
   end
 
   resources :sites
@@ -10,7 +10,7 @@ Gallery::Application.routes.draw do
   get '/users/login' => 'users#login', :as => :login_user
   get '/users/:provider/:user_key' => 'users#show', :as => :user
   delete '/users/current' => 'users#destroy', :as => :destory_current_user
-  
+
   get '/page/:page' => 'sites#index'
   root :to => 'sites#index'
 end
